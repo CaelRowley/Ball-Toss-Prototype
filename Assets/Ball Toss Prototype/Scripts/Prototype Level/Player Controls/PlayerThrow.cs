184d00b6986452a9d3a525a4d20f15f0d2c9f7c3
CaelRowley <caelrowley@gmail.com>
2016-06-07 15:03:33 +0100

    Initial Prototype
    Added:
    - plugins
    - Gvr SDK
    - Basic Scene for Ball Toss Prototype
    - Player throw script
    - Cup and Ball objects

---
+++ Assets/Ball Toss Prototype/Scripts/Prototype Level/Player Controls/PlayerThrow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed;
+
+    // Spawns projectile when the magnet switch is used
+    private void Update()
+    {
+        if(GvrViewer.Instance.Triggered)
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+        }
+    }
+}
